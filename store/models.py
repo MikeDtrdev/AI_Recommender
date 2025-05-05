0bd3a8c8429d4f1758ebb797636b66addbc8e931
MikeDtrdev <mikeddoctor08@gmail.com>
2025-05-04 22:53:31 -0800

    Initial project push

--- store/models.py
+++ store/models.py
@@ -4,7 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    tags = models.TextField(blank=True, null=True)  # Stored as string representation of list
+    tags = models.TextField(blank=True, null=True) 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
